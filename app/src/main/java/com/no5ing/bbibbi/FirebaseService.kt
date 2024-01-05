8b351fbdcf0b9aaab2986750cb0f30df29e1190e
송영민 <jakgon@naver.com>
2024-01-05 21:33:57 +0900

    feat: FCM 노티 구현

--- app/src/main/java/com/no5ing/bbibbi/FirebaseService.kt
+++ app/src/main/java/com/no5ing/bbibbi/FirebaseService.kt
@@ -1,5 +1,48 @@
 package com.no5ing.bbibbi
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.no5ing.bbibbi.presentation.ui.MainActivity
 
-class FirebaseService : FirebaseMessagingService()
+class FirebaseService : FirebaseMessagingService() {
+    companion object {
+        const val channel_id = "bbibbi_channel"
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        remoteMessage.notification?.apply {
+            val intent = Intent(this@FirebaseService, MainActivity::class.java).apply{
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent = PendingIntent
+                .getActivity(this@FirebaseService, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            val builder = NotificationCompat
+                .Builder(this@FirebaseService, channel_id)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            createNotificationChannel()
+            notificationManager.notify(101, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(channel_id, BuildConfig.notifyChannelName, importance)
+
+        val notificationManager: NotificationManager
+                = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.createNotificationChannel(channel)
+    }
+}
