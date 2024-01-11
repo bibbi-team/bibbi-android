8ea9d48274bd038796a5cb4adf3c3d5a7a00704d
ChuYong <jakgon@naver.com>
2024-01-11 11:08:36 +0900

    feat: 에러 메세지 추가

--- app/src/main/java/com/no5ing/bbibbi/util/ErrorMessageFactory.kt
+++ app/src/main/java/com/no5ing/bbibbi/util/ErrorMessageFactory.kt
@@ -12,6 +12,8 @@ fun getErrorMessage(errorCode: String?): String {
 
 fun Resources.getErrorMessage(errorCode: String?): String {
     return when (errorCode) {
+        "CM0002" -> getString(R.string.server_invalid_input)
+        "PO0001" -> getString(R.string.server_not_uploadable_time)
         "PO0002" -> getString(R.string.server_already_uploaded)
         else -> getString(R.string.server_unknown_error)
     }
