91d3df6ed2be4bb3dd0105066b7becb09e4ba250
송영민 <jakgon@naver.com>
2024-01-19 00:14:40 +0900

    chore: 앱 압축 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
