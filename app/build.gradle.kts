80bda0963b6d8147ae6b4fafd0ad27273c766e41
송영민 <jakgon@naver.com>
2024-01-24 19:57:05 +0900

    feat: 빌드 버전 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ val secretProperties = Properties().apply {
 val majorVersion = 1
 val minorVersion = 1
 val patchVersion = 0
-val buildVersion = 1
+val buildVersion = 3
 
 android {
     namespace = "com.no5ing.bbibbi"
