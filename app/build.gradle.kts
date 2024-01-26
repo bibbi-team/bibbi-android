f5c3a6b999dfb3834865d300a22e9accc276f69b
송영민 <jakgon@naver.com>
2024-01-26 23:26:38 +0900

    feat: 릴리즈 빌드버전업

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ val secretProperties = Properties().apply {
 val majorVersion = 1
 val minorVersion = 1
 val patchVersion = 0
-val buildVersion = 5
+val buildVersion = 6
 
 android {
     namespace = "com.no5ing.bbibbi"
@@ -92,7 +92,7 @@ android {
 dependencies {
     implementation(project(":widget"))
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.ui:ui")
