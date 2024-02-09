b33a6e78af31750958e4effb6ed1a04491b1e9d2
송영민 <jakgon@naver.com>
2024-02-09 17:51:18 +0900

    feat: 가족 링크 재가입 기능 추가

--- app/src/main/java/com/no5ing/bbibbi/presentation/feature/view/landing/already_family_exists/AlreadyFamilyExistsView.kt
+++ app/src/main/java/com/no5ing/bbibbi/presentation/feature/view/landing/already_family_exists/AlreadyFamilyExistsView.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -19,16 +24,27 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.no5ing.bbibbi.R
+import com.no5ing.bbibbi.data.repository.Arguments
 import com.no5ing.bbibbi.presentation.component.BBiBBiPreviewSurface
 import com.no5ing.bbibbi.presentation.component.BBiBBiSurface
 import com.no5ing.bbibbi.presentation.component.button.CTAButton
+import com.no5ing.bbibbi.presentation.feature.view.common.CustomAlertDialog
 import com.no5ing.bbibbi.presentation.theme.bbibbiScheme
 import com.no5ing.bbibbi.presentation.theme.bbibbiTypo
 
 @Composable
 fun AlreadyFamilyExistsView(
     onTapDispose: () -> Unit = {},
+    onTapQuitAndJoin: () -> Unit = {},
 ) {
+    val familyQuitModalEnabled = remember { mutableStateOf(false) }
+    CustomAlertDialog(
+        title = stringResource(id = R.string.dialog_quit_group_title),
+        description = stringResource(id = R.string.dialog_quit_group_message),
+        enabledState = familyQuitModalEnabled,
+        confirmMessage = stringResource(id = R.string.dialog_quit_group_confirm),
+        confirmRequest = onTapQuitAndJoin
+    )
     BBiBBiSurface(modifier = Modifier.fillMaxSize()) {
         Column(
             modifier = Modifier
@@ -58,21 +74,34 @@ fun AlreadyFamilyExistsView(
                 )
             }
             Image(
-                painter = painterResource(R.drawable.bbibbi),
+                painter = painterResource(R.drawable.sad_bbibbi),
                 contentDescription = null, // 필수 param
                 modifier = Modifier
+                    .size(width = 209.dp, height = 243.dp)
                     .fillMaxWidth(),
                 contentScale = ContentScale.FillWidth,
             )
-            CTAButton(
-                text = stringResource(id = R.string.already_family_exists_confirm),
-                onClick = onTapDispose,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 12.dp),
-                contentPadding = PaddingValues(vertical = 18.dp),
-            )
-
+            Column {
+                CTAButton(
+                    text = stringResource(id = R.string.already_family_exists_confirm),
+                    onClick = onTapDispose,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    contentPadding = PaddingValues(vertical = 18.dp),
+                )
+                Spacer(Modifier.height(12.dp))
+                CTAButton(
+                    text = stringResource(id = R.string.already_family_exists_exit_family_and_join),
+                    onClick = {
+                        familyQuitModalEnabled.value = true
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    contentPadding = PaddingValues(vertical = 18.dp),
+                )
+            }
         }
     }
 }
