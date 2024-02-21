8bd53f6f372ebb2ec1c86cf9eb922994e31a5eae
ChuYong <jakgon@naver.com>
2024-02-21 17:11:02 +0900

    feat: 버전 번호 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 11011
-        versionName = "1.1.1"
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
