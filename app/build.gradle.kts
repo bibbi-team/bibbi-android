f483cff448c4e8532e14cdb1d2c5cae97902c054
ChuYong <jakgon@naver.com>
2024-04-29 17:52:39 +0900

    feat: update version code [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 11011
-        versionName = "1.1.4"
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
